f453d3e39fed9756f6a63afc228f2198717b872a
Christoph Harlander <c.harlander@hotmail.com>
2020-11-12 16:16:49 +0100

    Models, Controllers, Views - finalised

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,36 @@
+class CocktailsController < ApplicationController
+  # before_action :find_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    # @doses = @cocktail.doses # see model -> a cocktail has many doses!
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Your cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # def find_cocktail
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  # end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, except: [ :destroy ]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail), notice: 'Your dose was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+end
