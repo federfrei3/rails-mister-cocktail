79269f77303e834ca520267f2ffd9ae9e82f50d0
Christoph Harlander <c.harlander@hotmail.com>
2020-11-12 13:03:29 +0100

    Models, Validations, Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+
+puts "Cleaning database now..."
+Ingredient.destroy_all
+puts "Database clean ✅"
+
+puts "Creating ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  item = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "#{item.name}"
+end
+
+puts "Done! Created #{Ingredient.count} ingredients."
